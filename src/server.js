587731ff0a50c1e96d0940d3ac75d0aff4f1f2a8
Mohd Monish <97455677+Mohd2002Monish@users.noreply.github.com>
2023-01-19 13:36:49 +0530

    intial commit

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const UserModel = require("./Model/user.model");
-
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const mongoose = require("mongoose");
 var bodyParser = require("body-parser");
 const res = require("express/lib/response");
@@ -13,10 +14,11 @@ app.get("/", (req, res) => {
 });
 app.post("/signup", async (req, res) => {
   const { name, email, pass } = req.body;
+  const hash = await bcrypt.hash(pass, 10);
   const user = new UserModel({
     name,
     email,
-    pass,
+    pass: hash,
   });
   await user.save();
   return res.status(201).send("User Created sucessfully");
@@ -25,8 +27,18 @@ app.post("/signin", async (req, res) => {
   const { email, pass } = req.body;
   const user = await UserModel.findOne({ email });
   if (!!user) {
-    if (user.pass == pass) {
-      return res.status(200).send("LOGIN SUCCESS");
+    const isPasswordCorrect = await bcrypt.compare(
+      pass,
+      user.pass
+    );
+    if (isPasswordCorrect) {
+      const token = jwt.sign(
+        { id: user._id, name: user.email },
+        "MONISH123"
+      );
+      return res
+        .status(200)
+        .send({ mess: "LOGIN SUCCESS", token: token });
     } else {
       res.send("password is incorrect");
     }
@@ -34,6 +46,17 @@ app.post("/signin", async (req, res) => {
     res.send("User not found");
   }
 });
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  if (token == null) return res.sendStatus(401);
+
+  jwt.verify(token, secret, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+}
 mongoose
   .connect(
     "mongodb+srv://mohdmonish:123@cluster0.lm1amsh.mongodb.net/mock-11"
